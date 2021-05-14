035cb712789b3acf904045f62be5332a920b9330
Pavlo Boiko <boiko.impul@gmail.com>
2021-05-15 02:25:14 +0300

    no message

--- Sources/FusionMedia_Apple/AudioPlayer+Apple.swift
+++ Sources/FusionMedia_Apple/AudioPlayer+Apple.swift
@@ -2,25 +2,67 @@ import FusionMedia_Common
 import AVFoundation
 
 public class AudioPlayer {
-  let url: URL    
-  var player: AVAudioPlayer? = nil  
-  
-  public required init(url: URL) {
-    self.url = url    
-  }
+    let url: URL
+    var player: AVAudioPlayer? = nil
+    var updater : CADisplayLink! = nil
+    var trackProgress: ((Float) -> Void)?
+    
+    public required init(url: URL) {
+        self.url = url
+        
+        player = try? AVAudioPlayer(contentsOf: self.url)
+        player?.prepareToPlay()
+    }
 }
 
 
 extension AudioPlayer: AudioPlayerProtocol {
-  public func play() {
-    player = try? AVAudioPlayer(contentsOf: self.url)
-    player?.prepareToPlay()
-    player?.play()
+    @objc func trackAudio() {
+        guard let player = player else {
+            trackProgress?(0)
+            return
+        }
+        trackProgress?(Float(player.currentTime / player.duration))
+    }
+    
+    public func play() {
+        updater = CADisplayLink(target: self, selector: #selector(self.trackAudio))
+        updater.preferredFramesPerSecond = 1
+        updater.add(to: .current, forMode: .common)
 
-  }
-  
-  public func stop() {
-     player?.stop()
-
-  }
+        player?.play()
+    }
+    
+    public func stop() {
+       player?.stop()
+    }
+    
+    public func isPlaying() -> Bool {
+        guard let player = player else { return false }
+        return player.isPlaying
+    }
+    
+    public func setProgress(_ progress: Float) {
+        guard let player = player else { return }
+        let isPlaying = player.isPlaying
+        player.stop()
+        player.currentTime = TimeInterval(Double(progress) * player.duration)
+        if isPlaying {
+            player.play()
+        }
+    }
+      
+    public func getProgress(_ progress: @escaping (_ progress: Float) -> Void) {
+        self.trackProgress = progress
+    }
+    
+    public func setVolume(_ volume: Float) {
+        guard let player = player else { return }
+        player.volume = volume
+    }
+    
+    public func getVolume() -> Float {
+        guard let player = player else { return 0 }
+        return player.volume
+    }
 }
--- Sources/FusionMedia_Common/AudioPlayer.swift
+++ Sources/FusionMedia_Common/AudioPlayer.swift
@@ -1,8 +1,13 @@
 import Foundation
 
 public protocol AudioPlayerProtocol {
-  init(url: URL)  
-  func play()
-  func stop()
+    init(url: URL)
+    func play()
+    func stop()
+    func isPlaying() -> Bool
+    func setProgress(_ progress: Float)
+    func getProgress(_ progress: @escaping (_ progress: Float) -> Void)
+    func setVolume(_ volume: Float)
+    func getVolume() -> Float
 }
 
