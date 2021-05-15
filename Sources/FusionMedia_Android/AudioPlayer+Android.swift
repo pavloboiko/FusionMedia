afffd10e8862d4cfa8b07ce86ccb723426530066
Pavlo Boiko <boiko.impul@gmail.com>
2021-05-15 23:54:47 +0300

    no message

--- Sources/FusionMedia_Android/AudioPlayer+Android.swift
+++ Sources/FusionMedia_Android/AudioPlayer+Android.swift
@@ -12,27 +12,57 @@ public class AudioPlayer {
   
   public required init(url: URL) {
     self.url = url
-  }     
-}
-
-extension AudioPlayer: AudioPlayerProtocol {
-  public func play() {
-    guard let isPlaying = self.player?.isPlaying(), !isPlaying else { return }
-    
     self.player = MediaPlayer()
     self.listener = MediaPlayerListener()        
             
     self.player?.setOnPreparedListener(listener: listener)
     self.player?.setDataSource(path: url.absoluteString)
     self.player?.prepareAsync()
-  }
+  }     
+}
+
+extension AudioPlayer: AudioPlayerProtocol {
+  	public func play() {
+    	guard let isPlaying = self.player?.isPlaying(), !isPlaying else { return }
+    
+		self.player?.start()
+  	}
   
-  public func stop() {
-  }  
+  	public func stop() {
+   	 	if (player?.isPlaying()) {      		player?.pause()    	}
+  	}
+  
+  	public func isPlaying() -> Bool {
+  		return player?.isPlaying()
+  	}
+  
+    public func getDuration() -> Double {
+        guard let player = player else { return 0 }
+        return Double(player.getDuration)
+    }
+    
+    public func setProgress(_ progress: Float) {
+        guard let player = player else { return }
+        player.seekTo(player.getDuration() * Int(progress))
+    }
+      
+    public func getProgress(_ progress: @escaping (_ progress: Float) -> Void) {
+
+    }
+    
+    public func setVolume(_ volume: Float) {
+        guard let player = player else { return }
+        player.setVolume(volume, volume)
+    }
+    
+    public func getVolume() -> Float {
+        guard let player = player else { return 0 }
+        return player.volume
+    }
 }
 
 class MediaPlayerListener: Object, MediaPlayer.OnPreparedListener {  
   func onPrepared(mp: MediaPlayer?) {     
-    mp?.start();
+//    mp?.start();
   }
 }
