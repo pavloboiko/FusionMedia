d6e29cbd32e1c3ac3eab1ab6944cda47671dc90d
Pavlo Boiko <boiko.impul@gmail.com>
2021-05-16 00:36:15 +0300

    no message

--- Sources/FusionMedia_Android/AudioPlayer+Android.swift
+++ Sources/FusionMedia_Android/AudioPlayer+Android.swift
@@ -5,34 +5,51 @@ import FusionMedia_Common
 
 
 public class AudioPlayer {
-  let url: URL
-  var player: MediaPlayer? = nil
-  var volume: Float = 1.0
+  	let url: URL
+  	var player: MediaPlayer? = nil
+  	var volume: Float = 1.0
   
-  private var listener: MediaPlayerListener? = nil
+    var timer:Timer!
+    var trackProgress: ((Float) -> Void)?
   
-  public required init(url: URL) {
-    self.url = url
-    self.player = MediaPlayer()
-    self.listener = MediaPlayerListener()
+  	private var listener: MediaPlayerListener? = nil
+  
+  	public required init(url: URL) {
+    	self.url = url
+    	self.player = MediaPlayer()
+    	self.listener = MediaPlayerListener()
     
-    self.player?.setVolume(leftVolume: volume, rightVolume: volume)            
-    self.player?.setOnPreparedListener(listener: listener)
-    self.player?.setDataSource(path: url.absoluteString)
-    self.player?.prepareAsync()
-  }     
+    	self.player?.setVolume(leftVolume: volume, rightVolume: volume)            
+    	self.player?.setOnPreparedListener(listener: listener)
+    	self.player?.setDataSource(path: url.absoluteString)
+    	self.player?.prepareAsync()
+  	}     
 }
 
 extension AudioPlayer: AudioPlayerProtocol {
+    @objc func trackAudio() {
+        guard let player = player else {
+            trackProgress?(0)
+            timer.invalidate()
+            return
+        }
+        trackProgress?(player.getCurrentPosition() / Float(self.getDuration()))
+        if !player.isPlaying {
+            timer.invalidate()
+        }
+    }
+    
   	public func play() {
     	guard let isPlaying = self.player?.isPlaying(), !isPlaying else { return }
-    
+    	timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector:  #selector(self.trackAudio), userInfo: nil, repeats: true)
 		self.player?.start()
   	}
   
   	public func stop() {
         guard let player = player else { return }
-   	 	if player.isPlaying() {      		player.pause()    	}
+   	 	if player.isPlaying() {
+      		player.pause()
+    	}
   	}
   
   	public func isPlaying() -> Bool {
@@ -51,7 +68,7 @@ extension AudioPlayer: AudioPlayerProtocol {
     }
       
     public func getProgress(_ progress: @escaping (_ progress: Float) -> Void) {
-
+        self.trackProgress = progress
     }
     
     public func setVolume(_ volume: Float) {
